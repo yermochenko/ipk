fa154a250b6313f00eeb1287d41efd91ccf40f30
Sergey Yermochenko <yermochenko@gmail.com>
2021-06-25 19:22:11 +0300

    add entity class

---
+++ src/Product.java
@@ -0,0 +1,16 @@
+public class Product {
+	private String name;
+	private Long price;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Long getPrice() {
+		return price;
+	}
+	public void setPrice(Long price) {
+		this.price = price;
+	}
+}
